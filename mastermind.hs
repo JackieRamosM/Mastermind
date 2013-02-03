1646afc846f187d7a6fba6ff387e96c4c9d996e3
Denny K. Schuldt <dschuldt@espol.edu.ec>
2013-02-03 17:18:17 -0500

    Generando adivinanza
    En esta actualización se obtiene un elemento de la población de posibles
    combinaciones. Se hace mediante un índice pseudo-aleatorio.

--- mastermind.hs
+++ mastermind.hs
@@ -1,34 +1,36 @@
-import Data.Char
+import System.Random
 import System.Time
 import Control.Exception
-import System.Random
+import Data.Char
+import System.IO.Unsafe
+
 mastermind = do
-                let
-                        letras = ['A','B','C','D','E','F']
-                        poblacion = [[w,x,y,z]|w<-letras,x<-letras,y<-letras,z<-letras]
-					
-                print poblacion
-				--randomico 5
-                hr'
-                putStr (take 35 (cycle " "))
-                putStr ("MASTERMIND")
-                putStr (take 35 (cycle " "))
-                hr'
-                br'
-                tab'
-                putStr "Nombre de jugador: "
-                nombre <- getLine
-                tab'
-                putStrLn ("Bienvenid@ " ++ nombre ++ "!")
-                br'
-                tab'
-                
-                putStrLn "-[ Retroalimentacion ]-"
-                putStr "Ingrese cuantas coinciden en letra y posicion: "
-                letrapos <- getLine
-                putStr "Ingrese cuantas coinciden en letra pero con diferente posicion: "
-                difpos <- getLine
-                putStr(comprueba(read letrapos, read difpos))
+		let letras = ['A','B','C','D','E','F']
+		let poblacion = [[w,x,y,z]|w<-letras,x<-letras,y<-letras,z<-letras]
+		let indice = randomico 5
+		let first = poblacion !! indice
+		br'
+		putStr("Yo digo que es " ++ first)
+		br'
+		hr'
+		putStr (take 35 (cycle " "))
+		putStr ("MASTERMIND")
+		putStr (take 35 (cycle " "))
+		hr'
+		br'
+		tab'
+		putStr "Nombre de jugador: "
+		nombre <- getLine
+		tab'
+		putStrLn ("Bienvenid@ " ++ nombre ++ "!")
+		br'
+		tab'		
+		putStrLn "-[ Retroalimentacion ]-"
+		putStr "Ingrese cuantas coinciden en letra y posicion: "
+		letrapos <- getLine
+		putStr "Ingrese cuantas coinciden en letra pero con diferente posicion: "
+		difpos <- getLine
+		putStr(comprueba(read letrapos, read difpos))
                 
 hr' = putStr (take 80 (cycle "*"))
 br' = putStrLn " "
@@ -38,24 +40,28 @@ comprueba :: (Int,Int) -> String
 comprueba (a,b)
     | (a,b) <= (0,0) = "Bien!"
     | (a,b) <= (1,0) = "Bien!"
-        | (a,b) <= (2,0) = "Bien!"
-        | (a,b) <= (3,0) = "Bien!"
-        | (a,b) <= (4,0) = "Bien!"
-        | (a,b) <= (0,1) = "Bien!"
-        | (a,b) <= (1,1) = "Bien!"
-        | (a,b) <= (2,1) = "Bien!"
-        | (a,b) <= (3,1) = "Bien!"
-        | (a,b) <= (0,2) = "Bien!"
-        | (a,b) <= (1,2) = "Bien!"
-        | (a,b) <= (2,2) = "Bien!"
-        | (a,b) <= (0,3) = "Bien!"
-        | (a,b) <= (1,3) = "Bien!"
-        | (a,b) <= (0,4) = "Bien!"
+	| (a,b) <= (2,0) = "Bien!"
+	| (a,b) <= (3,0) = "Bien!"
+	| (a,b) <= (4,0) = "Bien!"
+	| (a,b) <= (0,1) = "Bien!"
+	| (a,b) <= (1,1) = "Bien!"
+	| (a,b) <= (2,1) = "Bien!"
+	| (a,b) <= (3,1) = "Bien!"
+	| (a,b) <= (0,2) = "Bien!"
+	| (a,b) <= (1,2) = "Bien!"
+	| (a,b) <= (2,2) = "Bien!"
+	| (a,b) <= (0,3) = "Bien!"
+	| (a,b) <= (1,3) = "Bien!"
+	| (a,b) <= (0,4) = "Bien!"
     | otherwise      = "Combinacion invalida. (u_u)"
-    
-randomico :: Int -> Int
-randomico sec = head(take 1 . randomRs (0, 1296) . mkStdGen $ sec)       
-
+        
 gettime :: IO ClockTime
 gettime = getClockTime
-      
+ 
+getTiempo = do
+		t1d <- gettime
+		return (ctSec (toUTCTime t1d))
+	
+randomico :: Int -> Int
+randomico sec = head(take 1 . randomRs (0, 1296) . mkStdGen $ (unsafeDupablePerformIO (getTiempo)))  
+                        
