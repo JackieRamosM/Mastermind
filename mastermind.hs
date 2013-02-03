9de10f09f1aca4463e318ae7e66ceeb8b04f3fd8
JackieRamosM <jackie.ljrm@gmail.com>
2013-02-03 17:32:46 -0500

    El primer intento
    Se genera a partir de la funcion shuffle realizando otro array y tomando
    la cabeza.

--- mastermind.hs
+++ mastermind.hs
@@ -2,16 +2,17 @@ import System.Random
 import System.Time
 import Control.Exception
 import Data.Char
+import Data.Array.IO
+import Control.Monad
 import System.IO.Unsafe
 
 mastermind = do
 		let letras = ['A','B','C','D','E','F']
-		let poblacion = [[w,x,y,z]|w<-letras,x<-letras,y<-letras,z<-letras]
-		let indice = randomico 5
-		let first = poblacion !! indice
-		br'
-		putStr("Yo digo que es " ++ first)
-		br'
+		let poblacion = [[w,x,y,z]|w<-letras,x<-letras,y<-letras,z<-letras]	
+		let shuf = shuffle poblacion
+		--print (unsafeDupablePerformIO shuf)
+		let guess = take 1 (unsafeDupablePerformIO shuf)
+		--print guess
 		hr'
 		putStr (take 35 (cycle " "))
 		putStr ("MASTERMIND")
@@ -25,6 +26,9 @@ mastermind = do
 		putStrLn ("Bienvenid@ " ++ nombre ++ "!")
 		br'
 		tab'		
+		putStr "Mi adivinanza es: "
+		print guess
+		br'
 		putStrLn "-[ Retroalimentacion ]-"
 		putStr "Ingrese cuantas coinciden en letra y posicion: "
 		letrapos <- getLine
@@ -55,13 +59,17 @@ comprueba (a,b)
 	| (a,b) <= (0,4) = "Bien!"
     | otherwise      = "Combinacion invalida. (u_u)"
         
-gettime :: IO ClockTime
-gettime = getClockTime
- 
-getTiempo = do
-		t1d <- gettime
-		return (ctSec (toUTCTime t1d))
-	
-randomico :: Int -> Int
-randomico sec = head(take 1 . randomRs (0, 1296) . mkStdGen $ (unsafeDupablePerformIO (getTiempo)))  
+shuffle :: [a] -> IO[a]
+shuffle xs = do
+        ar <- newArray n xs
+        forM [1..n] $ \i -> do
+            j <- randomRIO (i,n)
+            vi <- readArray ar i
+            vj <- readArray ar j
+            writeArray ar j vi
+            return vj
+  where
+    n = length xs
+    newArray :: Int -> [a] -> IO (IOArray Int a)
+    newArray n xs =  newListArray (1,n) xs 
                         
